d6da3179ce07f258c8e5e3c68a483359fa6b18e8
Colby Chiang <cc2qe@virginia.edu>
2015-04-14 11:18:01 -0500

    checkpoint lumpy

--- examples/gtex_lumpy.sh
+++ examples/gtex_lumpy.sh
@@ -50,5 +50,73 @@ do
             -k"
 done
 
+# ----------------------------------------
+# 4. Get SV counts per sample
+# ----------------------------------------
+pwd
+# /gscmnt/gc2719/halllab/users/cchiang/projects/gtex/lumpy_2015-04-02/notes
+
+for SAMPLE in `cat /gscmnt/gc2719/halllab/users/cchiang/projects/gtex/lumpy_2015-04-02/notes/gtex_batch.txt | cut -f 1`
+do
+    zcat /gscmnt/gc2719/halllab/users/cchiang/projects/gtex/lumpy_2015-04-02/$SAMPLE/$SAMPLE.sv.vcf.gz \
+        | vawk -v S=$SAMPLE 'BEGIN {DEL=0; DUP=0; INV=0; BND=0} { if (I$SVTYPE=="DEL") DEL+=1; else if (I$SVTYPE=="DUP") DUP+=1; else if (I$SVTYPE=="INV") INV+=1; else if (I$SVTYPE=="BND" && ! I$SECONDARY) BND+=1 } END { print S,DEL,DUP,INV,BND }'
+done > /gscmnt/gc2719/halllab/users/cchiang/projects/gtex/lumpy_2015-04-02/notes/svtype_counts.txt
+
+# ----------------------------------------
+# 5. Merge SV VCFs
+# ----------------------------------------
+
+# l_sort cannot yet handle gzipped VCFs so let's unzip them first.
+for SAMPLE in `cat /gscmnt/gc2719/halllab/users/cchiang/projects/gtex/lumpy_2015-04-02/notes/gtex_batch.txt | cut -f 1`
+do
+    echo $SAMPLE
+    zcat /gscmnt/gc2719/halllab/users/cchiang/projects/gtex/lumpy_2015-04-02/$SAMPLE/$SAMPLE.sv.vcf.gz > /gscmnt/gc2719/halllab/users/cchiang/projects/gtex/lumpy_2015-04-02/$SAMPLE/$SAMPLE.sv.vcf
+done
+
+# concatenate and sort the variants
+echo -n /gscmnt/gc2719/halllab/users/cchiang/src/lumpy-sv/scripts/l_sort.py > sort_cmd.sh
+for SAMPLE in `cat /gscmnt/gc2719/halllab/users/cchiang/projects/gtex/lumpy_2015-04-02/merged_2015-04-09/merge_sample_list.txt`
+do
+    echo -ne " \\\\\n\t/gscmnt/gc2719/halllab/users/cchiang/projects/gtex/lumpy_2015-04-02/$SAMPLE/$SAMPLE.sv.vcf"
+done >> sort_cmd.sh
+bomb -m 25 -J lsort "bash sort_cmd.sh | bgzip -c > gtex_sorted_2015-04-09.sv.vcf.gz"
+
+# Collapse the variants into merged VCF
+# Warning: we are currently exploring ideal merging parameters.
+# Protocol subject to change.
+bomb -m 20 -J lmerge.$SLOP \
+    "zcat gtex_sorted_2015-04-09.sv.vcf.gz \
+        | python -u /gscmnt/gc2719/halllab/users/cchiang/src/lumpy-sv/scripts/l_merge.py -i /dev/stdin -f 10 -p 0.1 \
+        | bedtools sort -header \
+        | bgzip -c \
+        > gtex_merged.sv.vcf.gz"
+done
+
+# ----------------------------------------
+# 6. Genotype merged VCF with SVTyper
+# ----------------------------------------
+# Warning: this section is under active development
+
+# 
+
+# svtyper: 545e4854085d697de386cbd9c6cadaf689c3ea31
+for SAMPLE in `cat pilot10_samples.txt | cut -f 1`
+do
+    BAM=/gscmnt/gc2802/halllab/gtex_realign_2015-03-16/$SAMPLE/$SAMPLE.bam
+    SPL=/gscmnt/gc2802/halllab/gtex_realign_2015-03-16/$SAMPLE/$SAMPLE.splitters.bam
+    bomb -m 18 -J $SAMPLE.gt -g /cchiang/svtyper -o log/$SAMPLE.%J.log -e log/$SAMPLE.%J.log \
+    "zcat $VCF \
+        | vawk --header '{  \$6=\".\"; print }' \
+        | /gscmnt/gc2719/halllab/src/svtyper/svtyper \
+            -B $BAM \
+            -S $SPL \
+        > slop$SLOP/pilot10/$RUN/$SAMPLE.vcf"
+done
+
+
+
+
+
+
 
 
